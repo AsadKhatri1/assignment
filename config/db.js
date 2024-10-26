a4de52cc3b81e6fae2d5f611e980e68235e4e317
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-10-26 12:10:23 +0500

    fetching user through token in login

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://hafizasad398:dadabhai123@cluster0.3vklw.mongodb.net/task"
-    )
+    .connect(process.env.DB_URI)
     .then(() => {
       console.log("DB connected succesfully");
     })
